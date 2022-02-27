87e7a70093606401675e5552c760f9d44ced0a41
Lyss Garcia <alyssa.g200@gmail.com>
2022-02-26 17:33:46 -0800

    added signup.js, styled signup page, functionality? maybe

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,16 +1,50 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
+import cx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextInfoContent from '@mui-treasury/components/content/textInfo';
+import { useN04TextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/n04';
+import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
+// import { Link } from 'react-router-dom';
+// import { useMutation } from '@apollo/client';
 
 import Auth from '../utils/auth';
 import SignUpForm from '../components/SignUpForm'
 
+const useStyles = makeStyles(() => ({
+  root: {
+    maxWidth: 343,
+    margin: 'auto',
+    borderRadius: 12,
+    padding: 12,
+  },
+  media: {
+    borderRadius: 6,
+  },
+}));
+
 const Signup = () => {
-  
+
+  const styles = useStyles();
+  const textCardContentStyles = useN04TextInfoContentStyles();
+  const shadowStyles = useOverShadowStyles({ inactive: true });
+
   return (
+
       <main>
-    <h1>SIGN UP PAGE!!!</h1>
-    <SignUpForm/>
+    <Card className={cx(styles.root, shadowStyles.root)}>
+        <CardContent>
+          <TextInfoContent
+            classes={textCardContentStyles}
+            overline={'Welcome'}
+            heading={'Signup'}
+            body={
+              <SignUpForm/>
+            }
+          />
+        </CardContent>
+      </Card>
     </main>
   );
 };
