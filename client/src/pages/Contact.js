7541c9796753c8a90797b6c6f5a8bc364dd84de4
Lyss Garcia <alyssa.g200@gmail.com>
2022-03-01 20:54:51 -0800

    added footer w/ logo, contact and about page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,17 +4,19 @@ import { makeStyles } from '@material-ui/core/styles';
 import { motion } from 'framer-motion';
 import Card from '@material-ui/core/Card';
 import Box from '@mui/material/Box';
+import Button from '@material-ui/core/Button';
 import TextField from '@mui/material/TextField';
 import CardContent from '@material-ui/core/CardContent';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 import TextInfoContent from '@mui-treasury/components/content/textInfo';
+import { useBlogTextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/blog';
 import { useN04TextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/n04';
 import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
 
 
 const useStyles = makeStyles(() => ({
     root: {
-        maxWidth: 343,
+        maxWidth: 500,
         margin: 'auto',
         borderRadius: 12,
         padding: 12,
@@ -28,6 +30,7 @@ const Contact = () => {
     const styles = useStyles();
     const textCardContentStyles = useN04TextInfoContentStyles();
     const shadowStyles = useOverShadowStyles({ inactive: true });
+    const { button: buttonStyles } = useBlogTextInfoContentStyles();
 
     return (
         <motion.div
@@ -60,8 +63,9 @@ const Contact = () => {
                                     <TextareaAutosize
                                         aria-label="empty textarea"
                                         placeholder="..."
-                                        style={{ width: 200, height: 100 }}
+                                        style={{ width: 300, height: 100 }}
                                     />
+                                    <Button className={buttonStyles}>Send</Button>
                                 </Box>
                             }
                         />
