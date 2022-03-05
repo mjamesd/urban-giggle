fd118024d223dbbe63551013237f4cb5a7f7b3f8
Morgan Sherrill <mo.sherrill@outlook.com>
2022-03-04 23:59:41 -0800

    Hunt item flow is working!!!1

--- client/src/pages/HuntItem.js
+++ client/src/pages/HuntItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import cx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import { useParams } from 'react-router-dom';
@@ -13,12 +13,13 @@ import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
 import { useQuery } from '@apollo/client';
 import { useMutation } from '@apollo/client';
 import { GET_HUNT_ITEM, QUERY_ME } from '../utils/queries';
-import { USER_ASKS_FOR_HINT, CHANGE_POINTS } from '../utils/mutations';
+import { USER_ASKS_FOR_HINT } from '../utils/mutations';
+import Stack from '@mui/material/Stack';
 
-   
+
 const useStyles = makeStyles(() => ({
     root: {
-        maxWidth: 500,
+        Width: '250',
         margin: 'auto',
         borderRadius: 12,
         padding: 12,
@@ -27,6 +28,7 @@ const useStyles = makeStyles(() => ({
         borderRadius: 6,
     },
 }));
+
 
 const HuntItem = () => {
     const styles = useStyles();
@@ -40,92 +42,219 @@ const HuntItem = () => {
         // pass URL parameter
         variables: { huntItemId: huntItemId },
     });
-    const { data: userData, loading: userLoading } = useQuery(QUERY_ME) 
+    const { data: userData, loading: userLoading } = useQuery(QUERY_ME)
     const [userAsksForHint, { error: userAsksForHintError }] = useMutation(USER_ASKS_FOR_HINT);
 
     const huntItem = huntItemData?.huntItem || {};
     const currentUser = userData?.me || {};
+
+    if (huntItemLoading) {
+        return <h2>LOADING.....</h2>
+    }
+
+    if (userLoading) {
+        return <h2>LOADING.....</h2>
+    }
+
+
     console.log(huntItem, "HUNT ITEM")
     console.log(currentUser, "CURRENT USER!!")
 
-    const [hintState, setHintState] = useState({
-        _id: huntItemId,
-        hint2: hint2DisplayedTo,
-        hint3: huntItem.hint3,
-    });
+    const hintTwoDisplayedTo = huntItem.hint2DisplayedTo.map(user => user._id)
+    const hintThreeDisplayedTo = huntItem.hint3DisplayedTo.map(user => user._id)
+    const solutionDisplayed = huntItem.solutionDisplayedTo.map(user => user._id)
 
-    console.log(hintState)
 
-    const HintOneBody = () => {
-        return (
-            <div>
-                <h2>HINT NUMBER 1</h2>
-                <p>{huntItem.hint1}</p>
-                <Button className={buttonStyles} onClick={hintTwo}>Need another hint? (-1 pt)</Button>
-            </div>
-        )
-    }  
 
-    const HintTwoBody = () => {
-        return (
-            <div>
-                <h2>HINT NUMBER 1</h2>
-                <p>{huntItem.hint2}</p>
-                <Button className={buttonStyles} onClick={hintThree}>Need another hint? (-1 pt)</Button>
-            </div>
-        )
-    } 
 
-    const HintThreeBody = () => {
-        return (
-            <div>
-                <h2>HINT NUMBER 1</h2>
-                <p>{huntItem.hint3}</p>  
-                    <Button className={buttonStyles} onClick={huntItemSolution}>Need another hint? (-1 pt)</Button>
-            </div>
-        )
-    } 
+    const hintBody = () => {
+        
+        if (solutionDisplayed.includes(currentUser._id)) {
+            return (
+              <>  
+              <Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+            <CardContent>
+                <TextInfoContent
+                    classes={textCardContentStyles}
+                    overline={`Unknown ${huntItem.category}`}
+                    heading={`HINT NUMBER 1`}
+                    body={<div>
+                        <p>{huntItem.hint1}</p>
+                        
+                    </div>} />
+            </CardContent>
+        </Card>
+              <Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+                        <CardContent>
+                            <TextInfoContent
+                                classes={textCardContentStyles}
+                                overline={`Unknown ${huntItem.category}`}
+                                heading={`HINT NUMBER 2`}
+                                body={<div>
+                                    <p>{huntItem.hint2}</p>
+                                   
+                                </div>} />
+                        </CardContent>
+                    </Card><Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+                            <CardContent>
+                                <TextInfoContent
+                                    classes={textCardContentStyles}
+                                    overline={`Unknown ${huntItem.category}`}
+                                    heading={`HINT NUMBER 3`}
+                                    body={<div>
+                                        <p>{huntItem.hint3}</p>
+                                        
+                                    </div>} />
+                            </CardContent>
+                        </Card>
+            <Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+                <CardContent>
+                    <TextInfoContent
+                        classes={textCardContentStyles}
+                        overline={`Unknown ${huntItem.category}`}
+                        heading={`FINAL HINT`}
+                        body={<div>
+                            <p>{huntItem.solutionDescription}</p>
+                            <p>{huntItem.solutionLocation}</p>
+                        </div>} />
+                </CardContent>
+            </Card></>
+            )
+        }
+           
 
-    const DisplayHintTwo = async () => {
+        if (hintThreeDisplayedTo.includes(currentUser._id)) {
+                return( 
+                    <>
+                    <Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+            <CardContent>
+                <TextInfoContent
+                    classes={textCardContentStyles}
+                    overline={`Unknown ${huntItem.category}`}
+                    heading={`HINT NUMBER 1`}
+                    body={<div>
+                        <p>{huntItem.hint1}</p>
+                        
+                    </div>} />
+            </CardContent>
+        </Card>
+                    <Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+                        <CardContent>
+                            <TextInfoContent
+                                classes={textCardContentStyles}
+                                overline={`Unknown ${huntItem.category}`}
+                                heading={`HINT NUMBER 2`}
+                                body={<div>
+                                    <p>{huntItem.hint2}</p>
+                                    
+                                </div>} />
+                        </CardContent>
+                    </Card><Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+                            <CardContent>
+                                <TextInfoContent
+                                    classes={textCardContentStyles}
+                                    overline={`Unknown ${huntItem.category}`}
+                                    heading={`HINT NUMBER 3`}
+                                    body={<div>
+                                        <p>{huntItem.hint3}</p>
+                                        <Button className={buttonStyles} onClick={displaySolution}>Need Final Hint? (-1 pt)</Button>
+                                    </div>} />
+                            </CardContent>
+                        </Card></>
+            )
+        }
+
+
+        if (hintTwoDisplayedTo.includes(currentUser._id)) {
+            return( 
+                <><Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+                    <CardContent>
+                        <TextInfoContent
+                            classes={textCardContentStyles}
+                            overline={`Unknown ${huntItem.category}`}
+                            heading={`HINT NUMBER 1`}
+                            body={<div>
+                                <p>{huntItem.hint1}</p>
+                                
+                            </div>} />
+                    </CardContent>
+                </Card><Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+                        <CardContent>
+                            <TextInfoContent
+                                classes={textCardContentStyles}
+                                overline={`Unknown ${huntItem.category}`}
+                                heading={`HINT NUMBER 2`}
+                                body={<div>
+                                    <p>{huntItem.hint2}</p>
+                                    <Button className={buttonStyles} onClick={displayHintThree}>Need another hint? (-1 pt)</Button>
+                                </div>} />
+                        </CardContent>
+                    </Card></>
+             )
+         }
+
+         else {
+             return(
+            <Card style={{ width: '400px' }} className={cx(styles.root, shadowStyles.root)}>
+            <CardContent>
+                <TextInfoContent
+                    classes={textCardContentStyles}
+                    overline={`Unknown ${huntItem.category}`}
+                    heading={`HINT NUMBER 1`}
+                    body={<div>
+                        <p>{huntItem.hint1}</p>
+                        <Button className={buttonStyles} onClick={displayHintTwo}>Need another hint? (-1 pt)</Button>
+                    </div>} />
+            </CardContent>
+        </Card>
+             )
+         }
+        
+    }
+
+
+
+    const displayHintTwo = async () => {
         console.log("HINT ONE CLICKED!!!")
-        // const { data: hintData } = await userAsksForHint({
-        //     variables: { ...hintState,
-        //     points: deductPoint },
-        // });
+        const { data: hintData } = await userAsksForHint({
+            variables: { huntItemId: huntItemId, hint2: true },
+        });
+        console.log(hintData)
     }
 
     const displayHintThree = async () => {
         console.log("HINT TWO CLICKED!!!")
-        // const { data: hintData } = await userAsksForHint({
-        //     variables: { ...hintState,
-        //     points: deductPoint },
-        // });
+        const { data: hintData } = await userAsksForHint({
+            variables: { huntItemId: huntItemId, hint3: true },
+        });
+        console.log(hintData)
     }
 
-    if (huntItemLoading) {
-        return <h2>LOADING.....</h2>
-    } 
+    const displaySolution = async () => {
+        console.log("Solution CLICKED!!!")
+        const { data: hintData } = await userAsksForHint({
+            variables: { huntItemId: huntItemId, solution: true },
+        });
+        console.log(hintData)
+    }
+
 
 
 
     return (
+
         <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 2 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 1 }}
-    >
-        <main>
-                <Card className={cx(styles.root, shadowStyles.root)}>
-                    <CardContent>
-                        <TextInfoContent
-                        classes={textCardContentStyles}
-                            overline={'SEATTLE'}
-                            heading={'SECRET RESTAURANT'}
-                            body={<CardOneBody />} />
-                        />
-                    </CardContent>
-                </Card>
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 2 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+        >
+
+            <main>
+                <Stack spacing={2} justifyContent="center" alignItems="center">
+                    
+                   {hintBody()}
+                </Stack>
             </main>
         </motion.div>
     );
